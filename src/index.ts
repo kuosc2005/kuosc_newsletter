db42cafc32f2cc77326a20d423ae46586e430441
Sijan Bhandari <uhisijan@gmail.com>
2025-12-28 23:00:55 +0545

    refactor : interface changes and code refactor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import "dotenv/config";
-import { fetchData, fetchRepos } from "./controller/newsletter.controller";
+import {
+  fetchData,
+  fetchRepos,
+  getSummary,
+} from "./controller/newsletter.controller";
 import { GithubRepo, Summary } from "./types/github";
 import { sendDigestEmail } from "./sendDigest";
 
@@ -7,29 +11,7 @@ async function main() {
   const owner = "stdlib-js";
   try {
     const repos = await fetchRepos(owner);
-    const generatedAt = new Date().toISOString();
-    const summary: Summary[] = (
-      await Promise.all(
-        repos.map(async (repo) => {
-          const { issues, commits, pullRequests } = await fetchData(
-            owner,
-            repo.name,
-          );
-          if (!issues.length && !commits.length && !pullRequests.length)
-            return null;
-          return {
-            repoId: repo.id,
-            repoUrl: repo.url,
-            repoName: repo.name,
-            repoDescription: repo.description,
-            issues,
-            commits,
-            pullRequests,
-            generatedAt,
-          };
-        }),
-      )
-    ).filter((r): r is Summary => r !== null);
+    const summary: Summary[] = await getSummary(owner, repos);
     console.log(JSON.stringify(summary, null, 2));
     await sendDigestEmail(summary);
   } catch (error) {
