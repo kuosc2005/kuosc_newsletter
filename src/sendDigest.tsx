f98abdb431ab5893b064d7fa930d02cf427033a2
Sijan Bhandari <uhisijan@gmail.com>
2025-12-31 20:50:37 +0545

    refactor : removed extra files

--- src/sendDigest.tsx
+++ src/sendDigest.tsx
@@ -4,31 +4,4 @@ import { generateGitHubDigestEmail } from "./emails/GithubDigest";
 
 export async function sendDigestEmail(summary: Summary[]) {
   const html = generateGitHubDigestEmail(summary);
-
-  const generateTransporter = () => {
-    return {
-      service: "Gmail",
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: false,
-      auth: {
-        user: process.env.NODEMAILER_ACCOUNT,
-        pass: process.env.NODEMAILER_PASSWORD,
-      },
-    };
-  };
-
-  const transporter = nodemailer.createTransport(generateTransporter());
-  const mailOptions = {
-    from: process.env.NODEMAILER_ACCOUNT,
-    to: "uhisijan@gmail.com",
-    subject: "Welcome to ConnectNow",
-    html: html,
-  };
-  try {
-    await transporter.sendMail(mailOptions);
-    return true;
-  } catch (error) {
-    return false;
-  }
 }
